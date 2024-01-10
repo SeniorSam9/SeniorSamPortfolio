6e5c2830d1380b40dea877fd849e3d3ef133ae10
SeniorSam9 <rxrld050@hotmail.com>
2024-01-10 14:48:29 +0300

    basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants/constants";
+import { menu, close } from "../assets";
+import myLogo from "../assets/logo-no-bg.svg";
 
-const Navbar = () => {
+export default function Navbar() {
+  const [activeLink, setActiveLink] = useState("");
   return (
-    <div>Navbar</div>
-  )
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActiveLink("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={myLogo} alt="rayan-logo" />
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((item) => {
+            return (
+              <li>
+                <a href={`${item.id}`}>{item.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 }
-
-export default Navbar
