a6096db8336e8a2d515f574488207f9a67111c7e
SeniorSam9 <rxrld050@hotmail.com>
2024-01-10 17:17:47 +0300

    <Navbar /> done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import myLogo from "../assets/logo-no-bg.svg";
 
 export default function Navbar() {
   const [activeLink, setActiveLink] = useState("");
+  const [menuToggle, setMenuToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -25,12 +26,55 @@ export default function Navbar() {
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((item) => {
             return (
-              <li>
+              <li
+                key={item.id}
+                className={`${
+                  activeLink === item.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setActiveLink(item.title);
+                }}
+              >
                 <a href={`${item.id}`}>{item.title}</a>
               </li>
             );
           })}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={menuToggle ? close : menu}
+            alt="navbar-menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setMenuToggle(!menuToggle)}
+          />
+          {/*DROPDOWN MENU */}
+          <div
+            className={`${
+              !menuToggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((item) => {
+                return (
+                  <li
+                    key={item.id}
+                    className={`${
+                      activeLink === item.title
+                        ? "text-white"
+                        : "text-secondary"
+                    } font-poppins font-medium cursor-pointer text-[16px]`}
+                    onClick={() => {
+                      setActiveLink(item.title);
+                      setMenuToggle(!menuToggle);
+                    }}
+                  >
+                    <a href={`${item.id}`}>{item.title}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
