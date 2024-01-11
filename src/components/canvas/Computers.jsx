2068300b750f485039cc5646f1072ad1584157a9
SeniorSam9 <rxrld050@hotmail.com>
2024-01-11 16:22:35 +0300

    computercanvas done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
+export default function ComputerCanvas() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <div>Computers</div>
-  )
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      // position: [x, y, z]
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/**
+       * Suspense: loader component provided by React
+       */}
+      <Suspense fallback={<CanvasLoader />}>
+        {/**
+         * OrbitControls allows to rotate the object left and right
+         */}
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computer isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
 }
 
-export default Computers
+function Computer({ isMobile }) {
+  const computer = useGLTF("./desktop_pc_3d/scene.gltf");
+
+  return (
+    /*
+      Polygon mesh (vertices, edges, faces) 
+      */
+    <mesh>
+      {/**
+       * Lights
+       */}
+      <hemisphereLight intensity={0.25} />
+      <hemisphereLight intensity={1} />
+      <pointLight intensity={1} />
+
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      {/**
+       * The 3D object (3D computer)
+       */}
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+}
